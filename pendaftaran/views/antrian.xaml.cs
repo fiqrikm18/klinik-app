e3bd2d68ba15ac7b7078e164217ad22503d51873
fiqri khoirul m <fiqrikm18@gmail.com>
2019-05-06 18:46:06 +0700

    TODO: add fungsionalitas antrian

--- pendaftaran/views/antrian.xaml.cs
+++ pendaftaran/views/antrian.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using pendaftaran.DBAccess;
+using MySql.Data.MySqlClient;
+using pendaftaran.models;
 
 namespace pendaftaran.views
 {
@@ -20,9 +13,66 @@ namespace pendaftaran.views
     /// </summary>
     public partial class antrian : Page
     {
+        /* query getting data for datagrid
+         * --------------------------------------------------------------------------------------
+         * SELECT antrian.nomor_rm, pasien.nama, antrian.nomor_urut, poliklinik.nama_poliklinik
+         * FROM antrian
+         * LEFT JOIN pasien ON pasien.no_rekam_medis = antrian.nomor_rm
+         * LEFT JOIN poliklinik ON antrian.poliklinik = poliklinik.kode_poliklinik
+         * WHERE DATE(antrian.tanggal_berobat) = '2019-05-06';
+         * */
+
         public antrian()
         {
             InitializeComponent();
+
+            List<ComboboxPairs> cbp = new List<ComboboxPairs>();
+
+            try
+            {
+                if (DBConnection.dbConnection().State.Equals(System.Data.ConnectionState.Closed))
+                {
+                    DBConnection.dbConnection().Open();
+                    MySqlCommand command = new MySqlCommand("select * from poliklinik", DBAccess.DBConnection.dbConnection());
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cbp.Add(new ComboboxPairs(reader["nama_poliklinik"].ToString(), reader["kode_poliklinik"].ToString()));
+                        }
+                    }
+
+                    DBConnection.dbConnection().Close();
+                }
+                else
+                {
+                    MySqlCommand command = new MySqlCommand("select * from poliklinik", DBAccess.DBConnection.dbConnection());
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cbp.Add(new ComboboxPairs(reader["nama_poliklinik"].ToString(), reader["kode_poliklinik"].ToString()));
+                        }
+                    }
+
+                    DBConnection.dbConnection().Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi ke database gagal, periksa kembali database anda...\n" + ex.Message, "Perhatian", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            cbPoliklinik.DisplayMemberPath = "kode_poliklinik";
+            cbPoliklinik.SelectedValuePath = "nama_poliklinik";
+            cbPoliklinik.ItemsSource = cbp;
+            cbPoliklinik.SelectedIndex = 0;
+        }
+
+        private void TambahPasien(object sender, RoutedEventArgs e)
+        {
+            daftar_baru db = new daftar_baru();
+            NavigationService.Navigate(db);
         }
     }
 }
