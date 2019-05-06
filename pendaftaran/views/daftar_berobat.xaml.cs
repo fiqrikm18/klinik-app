ede96a79046a7bf67166dc98ba99b6e0dc8bb736
fiqri khoirul m <fiqrikm18@gmail.com>
2019-05-06 13:21:33 +0700

    TODO: daftar antrian pasien

--- pendaftaran/views/daftar_berobat.xaml.cs
+++ pendaftaran/views/daftar_berobat.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pendaftaran.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using pendaftaran.DBAccess;
+using MySql.Data.MySqlClient;
 
 namespace pendaftaran.views
 {
@@ -20,9 +23,111 @@ namespace pendaftaran.views
     /// </summary>
     public partial class daftar_berobat : Page
     {
+        MySqlCommand cmd;
+
         public daftar_berobat()
         {
             InitializeComponent();
+            List<ComboboxPairs> cbp = new List<ComboboxPairs>();
+
+            try
+            {
+                if (DBConnection.dbConnection().State.Equals(System.Data.ConnectionState.Closed))
+                {
+                    DBConnection.dbConnection().Open();
+                    MySqlCommand command = new MySqlCommand("select * from poliklinik", DBAccess.DBConnection.dbConnection());
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cbp.Add(new ComboboxPairs(reader["nama_poliklinik"].ToString(), reader["kode_poliklinik"].ToString()));
+                        }
+                    }
+
+                    DBConnection.dbConnection().Close();
+                }
+                else
+                {
+                    MySqlCommand command = new MySqlCommand("select * from poliklinik", DBAccess.DBConnection.dbConnection());
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cbp.Add(new ComboboxPairs(reader["nama_poliklinik"].ToString(), reader["kode_poliklinik"].ToString()));
+                        }
+                    }
+
+                    DBConnection.dbConnection().Close();
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Koneksi ke database gagal, periksa kembali database anda...\n" + ex.Message, "Perhatian", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            cbPoliklinik.DisplayMemberPath = "kode_poliklinik";
+            cbPoliklinik.SelectedValuePath = "nama_poliklinik";
+            cbPoliklinik.ItemsSource = cbp;
+            cbPoliklinik.SelectedIndex = 0;
+        }
+
+        private void tambah_antrian(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("as");
+            ComboboxPairs cbp = (ComboboxPairs)cbPoliklinik.SelectedItem;
+            string policode = cbp.nama_poliklinik;
+            string poliklinik = policode;
+            //string norm = txtIdPasien.Text.ToString();
+            string norm = "444";
+
+            try
+            {
+                if (DBConnection.dbConnection().State.Equals(System.Data.ConnectionState.Closed))
+                    DBConnection.dbConnection().Open();
+
+                string query = "select count(*) from pasien where no_rekam_medis = '" + norm + "';";
+                cmd = new MySqlCommand(query, DBAccess.DBConnection.dbConnection());
+                int rm_exist = int.Parse(cmd.ExecuteScalar().ToString());
+                DBConnection.dbConnection().Close();
+
+                if (rm_exist >= 1)
+                {
+                    string last = "";
+                    int a = 0;
+                    //int.TryParse(last, out a);
+                    int no_urut = 0;
+                    string query_last = "select nomor_urut from antrian where poliklinik= '" + policode + "' and DATE(tanggal_berobat) = '" + DateTime.Now.ToString("yyyy-MM-dd") + "' ORDER BY nomor_urut desc LIMIT 1;";
+                    //string query_last = "select nomor_urut from antrian where poliklinik= '003' and DATE(tanggal_berobat) = '" + DateTime.Now.ToString("yyyy-MM-dd") + "' ORDER BY nomor_urut desc LIMIT 1;";
+                    cmd = new MySqlCommand(query_last, DBAccess.DBConnection.dbConnection());
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read()) a = reader.GetInt32(0);
+
+                    if (last != null || last != "") no_urut = a + 1;
+                    else no_urut = 1;
+
+                    DBConnection.dbConnection().Close();
+                    DBConnection.dbConnection().Open();
+                    query = "insert into antrian(nomor_rm, nomor_urut, poliklinik) values('" + norm + "','" + no_urut + "','" + policode + "');";
+                    cmd = new MySqlCommand(query, DBAccess.DBConnection.dbConnection());
+
+                    int res = cmd.ExecuteNonQuery();
+
+                    if (res == 1)
+                        MessageBox.Show("Pasien berhasil ditambahkan. \nNomor antri: " + no_urut, "Informasi", MessageBoxButton.OK, MessageBoxImage.Information);
+                    //MessageBox.Show(last);
+                    else
+                        MessageBox.Show("Data pasien berhasil ditambahkan. \nGagal menambahakan pasien ke antrian.", "Informasi", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Rekam medis pasien belum teraftar, periksa kemabali data pasien.", "Perhatian", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi ke database gagal, periksa kembali database anda...\n" + ex.Message, "Perhatian", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
         }
     }
 }
